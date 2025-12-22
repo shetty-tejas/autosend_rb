99cac5f0fc117505615c3f5c4a7dbb05dde71712
Tejas <74608924+shetty-tejas@users.noreply.github.com>
2025-12-22 11:21:26 +0530

    Adding SimpleCov Support to CI (#3)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
 require "simplecov"
+require "simplecov_json_formatter"
+
 SimpleCov.start do
   add_filter "/test/"
+
+  enable_coverage :branch
+
+  minimum_coverage 90
+  minimum_coverage_by_file 40
+
+  formatter SimpleCov::Formatter::MultiFormatter.new([
+                                                       SimpleCov::Formatter::HTMLFormatter,
+                                                       SimpleCov::Formatter::JSONFormatter
+                                                     ])
 end
 
 require "autosend_rb"
